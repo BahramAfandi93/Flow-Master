081aa1ee4d4001c5924d63dd9197d84df664898f
bahramafandi <bahram.afandi@pashabank.az>
2025-08-21 10:22:24 +0400

    FM-10: Simple calculation endpoints

--- src/main/kotlin/com/calcpro/flowmaster/controller/CalculationController.kt
+++ src/main/kotlin/com/calcpro/flowmaster/controller/CalculationController.kt
@@ -1,8 +1,8 @@
 package com.calcpro.flowmaster.controller
 
-import com.calcpro.flowmaster.dao.entity.Structure
 import com.calcpro.flowmaster.dto.CulvertRequest
 import com.calcpro.flowmaster.service.CalculationService
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController
 // this controller is for MVP and will be used to calculate the structure for MVP stage
 // will be replaced in feature
 
+@CrossOrigin
 @RestController
 @RequestMapping("/simple/calculate")
 class CalculationController(
@@ -19,7 +20,5 @@ class CalculationController(
     @PostMapping("/culvert")
     fun calculateCircleStructure(
         @RequestBody culvertRequest: CulvertRequest
-    ): Structure {
-        return calculationService.calculateCulvert(culvertRequest)
-    }
+    ) = calculationService.calculateCulvert(culvertRequest)
 }
